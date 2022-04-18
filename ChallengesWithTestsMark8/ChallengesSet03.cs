9a20de1a52074d3c391e27e9eb92435d116f8d51
Hannah Willis <hwillis8817@gmail.com>
2022-04-17 23:51:49 -0500

    completed wk 3 but i cant run due to error somewhere

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,61 +1,153 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet03
     {
         public bool ArrayContainsAFalse(bool[] vals)
-        {
-           for (int i = 0; i < vals.Length; i++)    
+        { if (vals == null)
             {
-                if (vals [i] == false)
+                return false;
+
+            }
+
+            for (int i = 0; i < vals.Length; i++)
+            {
+                if (vals[i] == false)
                 {
                     return true;
                 }
 
             }
-           return false;
+
+            return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return false;
+            }
+
+            if (numbers.Sum() % 2 != 0)
+            {
+                return true;
+            }
+
+            return false;
+
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
-        }
+            int validConditions = 0;
+            foreach (char c in password)
+            {
+                if (c >= 'a' && c <= 'z')
+                {
+                    validConditions++;
+                    break;
+                }
+                {
+                    return true;
+                }
+            } return false;
 
-        public char GetFirstLetterOfString(string val)
-        {
-            throw new NotImplementedException();
-        }
+            foreach (char c in password)
+            {
+                if (c >= 'A' && c <= 'Z')
+                {
+                    validConditions++;
+                    break;
+                }
+                {
+                    return true;
+                }
+            } return false;
 
-        public char GetLastLetterOfString(string val)
-        {
-            throw new NotImplementedException();
-        }
+            if (validConditions == 0) return false;
 
-        public decimal Divide(decimal dividend, decimal divisor)
-        {
-            throw new NotImplementedException();
-        }
+            foreach (char c in password)
+            {
+                if (c >= '0' && c <= '9')
+                {
+                    validConditions++;
+                    break;
+                    {
+                        return true;
+                    }
+                }
+                return false;
 
-        public int LastMinusFirst(int[] nums)
-        {
-            throw new NotImplementedException();
-        }
+            }
 
-        public int[] GetOddsBelow100()
-        {
-            throw new NotImplementedException();
-        }
+            public char GetFirstLetterOfString(string val)
+            {
+                // str = str.Substring(n);//
 
-        public void ChangeAllElementsToUppercase(string[] words)
-        {
-            throw new NotImplementedException();
+                char firstCharacter = str[0];
+                Console.WriteLine(firstCharacter);
+            }
+
+            public char GetLastLetterOfString(string val)
+
+
+            {
+                return char lastCharacter = str[0];
+
+            }
+
+            public decimal Divide(decimal dividend, decimal divisor)
+            {
+                if (divisor == 0 || dividend == 0)
+                {
+                    return 0;
+                }
+
+                return dividend / divisor;
+
+
+
+                public int LastMinusFirst(int[] nums)
+                {
+                    return nums[nums.Length - 1] - nums[0];
+                }
+
+                public int[] GetOddsBelow100()
+                {
+                    int[] nums = new int[100];
+
+                    for (int i = 0; i < nums.Length; i++)
+                    {
+                        //condition to check ODD number
+                        if (nums[i] % 2 != 0)
+
+                        {
+                            return nums;
+
+                        }
+
+                    }
+
+                }
+
+                public void ChangeAllElementsToUppercase(string[] words)
+                {
+                    for (int i = 0; i < words.Length; i++)
+                    {
+                        words[i] = words[i].ToUpper();
+                    }
+
+                }
+            }
         }
     }
+
 }
+
+
+
+
